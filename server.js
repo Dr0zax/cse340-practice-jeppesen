e5b16e5f4240b61421242159f8cc83c637866406
Dr0zax <geodashfan4@gmail.com>
2025-09-19 14:54:04 -0600

    Add initial views and partials for the Express app
    - Created about.ejs with personal information and hobbies.
    - Created home.ejs as the landing page with a welcome message.
    - Added footer.ejs for consistent footer across pages.
    - Added header.ejs for navigation links and page structure.
    - Created products.ejs for future product listings.

--- server.js
+++ server.js
@@ -1,18 +1,33 @@
+import {fileURLToPath} from 'url';
+import path from 'path';
 import express from 'express';
 
 const app = express();
 
-const name = process.env.NAME;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const NAME = process.env.NAME;
+const PORT = process.env.PORT || 3000;
+const NODE_ENV = process.env.NODE_ENV || "production";
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.send(`Hello, ${name}!`);
+    const title = "Welcome!";
+    res.render('home', { title });
+})
+app.get('/about', (req, res) => {
+    const title = "About Me";
+    res.render('about', { title });
+})
+app.get('/products', (req, res) => {
+    const title = "Products";
+    res.render('products', { title })
 })
 
-app.get("/new-route", (req, res) => {
-    res.send("This is a different route.");
-})
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'src/views'));
 
-const port = 3000;
-app.listen(port, () => {
-    console.log(`Server, is running on http://127.0.0.1:${port}`);
+app.listen(PORT, () => {
+    console.log(`Server, is running on http://127.0.0.1:${PORT}`);
 })
