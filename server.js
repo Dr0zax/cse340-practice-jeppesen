3e88b162daf544246bf7ef55ddc8a14ae4ca4521
Dr0zax <geodashfan4@gmail.com>
2025-09-22 14:07:35 -0600

    Add WebSocket support and update environment handling
    - Introduced WebSocket server for development environment in server.js
    - Updated header.ejs to include WebSocket client script
    - Added ws dependency to package.json and pnpm-lock.yaml
    - Modified about.ejs to correct punctuation
    - Enhanced nodemon.json with script for development

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import {fileURLToPath} from 'url';
 import path from 'path';
 import express from 'express';
+import { error } from 'console';
 
 const app = express();
 
@@ -11,6 +12,11 @@ const PORT = process.env.PORT || 3000;
 const NODE_ENV = process.env.NODE_ENV || "production";
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+    res.locals.NODE_ENV = NODE_ENV.toLowerCase() || 'production';
+    next();
+})
 
 app.get('/', (req, res) => {
     const title = "Welcome!";
@@ -27,6 +33,24 @@ app.get('/products', (req, res) => {
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'src/views'));
+
+if (NODE_ENV.includes('dev')) {
+    const ws = await import('ws');
+
+    try {
+        const wsPort = parseInt(PORT) + 1;
+        const wsServer = new ws.WebSocketServer({ port: wsPort });
+        wsServer.on('listening', () => {
+            console.log(`Websocket running on port ${wsPort}`);
+        })
+
+        wsServer.on('error', (error) => {
+            console.error('Websocket server error:', error);
+        })
+    } catch (error) {
+        console.error("Failed to start WebSocket server", error);
+    }
+}
 
 app.listen(PORT, () => {
     console.log(`Server, is running on http://127.0.0.1:${PORT}`);
